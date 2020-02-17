4bca84fb40079b3724c57ccdce5af9abfe2dcf2c
jayhebe <jayhebe1983@sina.com>
2020-02-17 23:18:49 +0800

    Intermediate exercises 20200217

---
+++ src/com/exercises/io/FileExer.java
@@ -0,0 +1,72 @@
+package com.exercises.io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileExer
+{
+    public static void main(String[] args) throws IOException
+    {
+        File fileDir = new File("D:\\ioexer");
+        fileDir.mkdir();
+
+        File testDir1 = new File(fileDir.getAbsolutePath() + File.separator + "io1");
+        File testDir2 = new File(fileDir.getAbsolutePath() + File.separator + "io2");
+        File testDir3 = new File(fileDir.getAbsolutePath() + File.separator + "io3");
+//        System.out.println(testDir1);
+
+//        testDir1.mkdir();
+//        testDir2.mkdir();
+//        testDir3.mkdir();
+
+        File testFile1 = new File("D:\\ioexer\\text1.txt");
+        File testFile2 = new File("D:\\ioexer\\text1.jpg");
+        File testFile3 = new File("D:\\ioexer\\text2.jpg");
+
+//        if (!testFile1.exists())
+//        {
+//            testFile1.createNewFile();
+//        }
+//
+//        if (!testFile2.exists())
+//        {
+//            testFile2.createNewFile();
+//        }
+//
+//        if (!testFile3.exists())
+//        {
+//            testFile3.createNewFile();
+//        }
+
+//        if (deleteFile(fileDir, "text1.txt"))
+//        {
+//            System.out.println("Deleted successful.");
+//        }
+
+        listFiles("D:\\ioexer", ".jpg");
+    }
+
+    public static boolean deleteFile(File file, String fileName)
+    {
+        boolean result = false;
+        File delFile = new File(file, fileName);
+        if (delFile.exists())
+        {
+            result = delFile.delete();
+        }
+        return result;
+    }
+
+    public static void listFiles(String directory, String extension)
+    {
+        File dir = new File(directory);
+        String[] allFiles = dir.list();
+        for (String file : allFiles)
+        {
+            if (file.endsWith(extension))
+            {
+                System.out.println(file);
+            }
+        }
+    }
+}
---
+++ src/com/exercises/io/FileTest.java
@@ -0,0 +1,99 @@
+package com.exercises.io;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest
+{
+    public static void main(String[] args) throws IOException
+    {
+        File file1 = new File("hello.txt");
+        File file2 = new File("D:\\io\\hello.txt");
+        File file3 = new File("D:\\io", "io1");
+        File file4 = new File(file3, "hi.txt");
+        File file5 = new File("D:\\io\\hi.txt");
+
+//        System.out.println(file1);
+//        System.out.println(file2);
+//        System.out.println(file3);
+//        System.out.println(file4);
+//
+//        System.out.println(file1.getAbsolutePath());
+//        System.out.println(file1.getPath());
+//        System.out.println(file1.getName());
+//        System.out.println(file1.getParent());
+//        System.out.println(file1.length());
+//        System.out.println(new Date(file1.lastModified()));
+//
+//        System.out.println("---------------------------------------------");
+//
+//        System.out.println(file2.getAbsolutePath());
+//        System.out.println(file2.getPath());
+//        System.out.println(file2.getName());
+//        System.out.println(file2.getParent());
+//        System.out.println(file2.length());
+//        System.out.println(new Date(file2.lastModified()));
+//
+//        String[] list = file3.list();
+//        for (String f : list)
+//        {
+//            System.out.println(f);
+//        }
+//
+//        System.out.println("---------------------------------------------");
+//
+//        File[] files = file3.listFiles();
+//        for (File file : files)
+//        {
+//            System.out.println(file);
+//        }
+
+//        boolean b = file1.renameTo(file5);
+//        System.out.println(b);
+
+//        System.out.println("---------------------------------------------");
+//
+//        System.out.println(file1.isDirectory());
+//        System.out.println(file1.isFile());
+//        System.out.println(file1.exists());
+//        System.out.println(file1.canRead());
+//        System.out.println(file1.canWrite());
+//        System.out.println(file1.isHidden());
+//
+//        System.out.println("---------------------------------------------");
+//
+//        System.out.println(file3.isDirectory());
+//        System.out.println(file3.isFile());
+//        System.out.println(file3.exists());
+//        System.out.println(file3.canRead());
+//        System.out.println(file3.canWrite());
+//        System.out.println(file3.isHidden());
+//
+//        System.out.println("---------------------------------------------");
+
+//        if (!file4.exists())
+//        {
+//            file4.createNewFile();
+//            System.out.println("Created successful.");
+//        }
+//        else
+//        {
+//            file4.delete();
+//            System.out.println("Deleted successful.");
+//        }
+
+        File file6 = new File("D:\\io\\io1");
+
+        boolean mkdir = file6.mkdir();
+        if (mkdir)
+        {
+            System.out.println("hehehe");
+        }
+
+        boolean mkdirs = file6.mkdirs();
+        if (mkdirs)
+        {
+            System.out.println("hahaha");
+        }
+    }
+}
